990195509be8d77e832bd6622e8347cbdf298cc5
elbarto120 <gael.magana@tecdesoftware.edu.mx>
2026-05-19 12:50:51 -0600

    Add project files.

---
+++ Catalogo.Presentacion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catalogo.Presentacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
